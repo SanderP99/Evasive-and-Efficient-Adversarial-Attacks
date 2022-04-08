c523d8f9717cf04b0eb0ea5ec3d001bf55373f58
Sander <prenen.sander@gmail.com>
2022-04-08 17:14:11 +0200

    Threat model writing

--- Code/Experiments/DistributedBBA/get_detections.py
+++ Code/Experiments/DistributedBBA/get_detections.py
@@ -25,52 +25,57 @@ if __name__ == '__main__':
     #          'n_calls',
     #          'detections_per_node', 'distribution_scheme'])
 
-    n_experiments = 1
+    n_experiments = 2
     n_particles = 5
     max_queries = 25000
-    for i in range(n_experiments):
+    for i in [3]:
         for n_nodes in [1, 5]:
-            np.random.seed(42)
-            if n_nodes == 1:
-                mapping = deque([0] * n_particles)
-            elif n_nodes == 5:
-                mapping = deque(range(n_particles))
-            else:
-                mapping = None
+            for insert_noise in ['uniform', 'perlin', 'mixed']:
+                for n in [1, 10, 20]:
+                    np.random.seed(42)
+                    if n_nodes == 1:
+                        mapping = deque([0] * n_particles)
+                    elif n_nodes == 5:
+                        mapping = deque(range(n_particles))
+                    else:
+                        mapping = None
 
-            experiment = experiments.iloc[i]
-            x_orig = mnist.test_data[experiment.name]
+                    experiment = experiments.iloc[i]
+                    x_orig = mnist.test_data[experiment.name]
 
-            targets = ast.literal_eval(experiment.targets)
-            random_inits = mnist.test_data[
-                np.array(targets)[np.random.choice(len(targets), size=n_particles, replace=False)]]
-            distribution_scheme = RoundRobinDistribution(mapping, n_nodes=n_nodes, n_particles=n_particles)
-            # distribution_scheme = DistanceBasedDistributionScheme(mapping, n_nodes, dataset='mnist')
-            attack = InsertNoiseDistributedBiasedBoundaryAttack(n_particles=n_particles, model=bb_model,
-                                                                target_img=x_orig,
-                                                                target_label=experiment.y_target, inits=random_inits,
-                                                                distribution_scheme=distribution_scheme,
-                                                                n_nodes=n_nodes, mapping=mapping, dataset='mnist',
-                                                                insert_every=10, insert_n=5, insert_noise='uniform')
-            previous_queries = 0
-            new_queries = 0
-            with tqdm(total=max_queries) as pbar:
-                while attack.swarm.total_queries < max_queries:
-                    attack.attack()
-                    new_queries, previous_queries = attack.swarm.total_queries, new_queries
-                    pbar.update(new_queries - previous_queries)
+                    targets = ast.literal_eval(experiment.targets)
+                    random_inits = mnist.test_data[
+                        np.array(targets)[np.random.choice(len(targets), size=n_particles, replace=False)]]
+                    distribution_scheme = RoundRobinDistribution(mapping, n_nodes=n_nodes, n_particles=n_particles)
+                    # distribution_scheme = DistanceBasedDistributionScheme(mapping, n_nodes, dataset='mnist')
+                    attack = InsertNoiseDistributedBiasedBoundaryAttack(n_particles=n_particles, model=bb_model,
+                                                                        target_img=x_orig,
+                                                                        target_label=experiment.y_target,
+                                                                        inits=random_inits,
+                                                                        distribution_scheme=distribution_scheme,
+                                                                        n_nodes=n_nodes, mapping=mapping,
+                                                                        dataset='mnist',
+                                                                        insert_every=n, insert_n=5,
+                                                                        insert_noise=insert_noise)
+                    previous_queries = 0
+                    new_queries = 0
+                    with tqdm(total=max_queries) as pbar:
+                        while attack.swarm.total_queries < max_queries:
+                            attack.attack()
+                            new_queries, previous_queries = attack.swarm.total_queries, new_queries
+                            pbar.update(new_queries - previous_queries)
 
-            # for _ in tqdm(range(1000)):
-            #     attack.attack()
+                    # for _ in tqdm(range(1000)):
+                    #     attack.attack()
 
-            detections_all = [node.detector.get_detections() for node in attack.nodes]
-            total_detections = np.sum([len(x) for x in detections_all])
-            print(total_detections)
+                    detections_all = [node.detector.get_detections() for node in attack.nodes]
+                    total_detections = np.sum([len(x) for x in detections_all])
+                    print(total_detections)
 
-            with open(output_file, 'a') as file:
-                writer = csv.writer(file)
-                writer.writerow(
-                    [experiment.name, experiment.y_orig, experiment.y_target, n_particles, n_nodes,
-                     attack.swarm.best_fitness,
-                     total_detections, attack.swarm.total_queries, [len(x) for x in detections_all],
-                     str(distribution_scheme) + ' insert_20_uniform', 'mnist'])
+                    with open(output_file, 'a') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(
+                            [experiment.name, experiment.y_orig, experiment.y_target, n_particles, n_nodes,
+                             attack.swarm.best_fitness,
+                             total_detections, attack.swarm.total_queries, [len(x) for x in detections_all],
+                             str(distribution_scheme) + f' insert_{n}_' + insert_noise, 'mnist'])
